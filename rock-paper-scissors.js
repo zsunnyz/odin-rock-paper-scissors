c847c216de0610a67a2b20cbf6298ee41edd1815
Sunny Zhang <zhasunny0001@gmail.com>
2022-06-03 16:05:42 +1000

    added html elements for rps and modified js to change the text contents of divs

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,3 +1,11 @@
+let resultContainer = document.querySelector('.result');
+let selectionsContainer = document.querySelector('.selections');
+let playerContainer = document.querySelector('.player-score');
+let computerContainer = document.querySelector('.computer-score');
+
+let playerScore = 0;
+let computerScore = 0;
+
 function computerPlay(){
     let randomVal = Math.floor(Math.random() * 3);
     switch(randomVal){
@@ -10,27 +18,29 @@ function computerPlay(){
     }
 }
 
-function playRound(playerSelection, computerSelection){
-    playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();   
-    computerSelection = computerSelection[0].toUpperCase() + computerSelection.slice(1).toLowerCase();
-    console.log(`You chose ${playerSelection}`)
-    console.log(`Computer chose ${computerSelection}`)
-    
-
-    if((playerSelection == 'Rock' && computerSelection == 'Scissors') || (playerSelection == 'Paper' && computerSelection == 'Rock') || (playerSelection == 'Scissors' && computerSelection == 'Paper')){
-        return `You win! ${playerSelection} beats ${computerSelection}}`;
-    }
-    else if (playerSelection == computerSelection){
-        return 'You drew!'
-    }
-    else {
-        return `You Lose! ${playerSelection} beats ${computerSelection}}`
-    }
+function updateScore() {
+    playerContainer.textContent = playerScore;
+    computerContainer.textContent = computerScore;
 }
 
-function game(){
-    for (let i = 0; i < 5; i ++){
-        let playerSelection = prompt("Enter rock, paper or scissors to play!");
-        console.log(playRound(playerSelection, computerPlay()));
+function playRound(playerSelection, computerSelection){
+    playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
+    computerSelection = computerSelection[0].toUpperCase() + computerSelection.slice(1).toLowerCase();
+     
+    if((playerSelection == 'Rock' && computerSelection == 'Scissors') || (playerSelection == 'Paper' && computerSelection == 'Rock') || (playerSelection == 'Scissors' && computerSelection == 'Paper')){
+        resultContainer.textContent = 'You Win!';
+        selectionsContainer.textContent = `${playerSelection} beats ${computerSelection}`;
+        playerScore += 1;
     }
+    else if (playerSelection == computerSelection){
+        resultContainer.textContent = 'You drew!';
+        selectionsContainer.textContent = `You both chose ${playerSelection}`
+    }
+    else {
+        resultContainer.textContent = 'You Lose!';
+        selectionsContainer.textContent = `${playerSelection} beats ${computerSelection}`;
+        computerScore += 1;
+    }
+    
+    updateScore();
 }
